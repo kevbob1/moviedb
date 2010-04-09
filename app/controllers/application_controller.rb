50272c19387d74cc4673221f4834832243024a41
kev <kev@laptop1>
2010-04-09 00:24:39 -0600

    update w/ new style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,18 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+	layout "application"
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+	def home
+    @movies = []
+		Movie.all.each do |row|
+			@movies << Movie.new(row)
+		end
+
+	end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -85,4 +85,8 @@ class MoviesController < ApplicationController
       format.xml  { head :ok }
     end
   end
+
+	def search
+		
+	end
 end
