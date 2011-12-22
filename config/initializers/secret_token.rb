ea6a369d7ad297010ffcb86c03e32f7329b4e700
Kevin <kev@drule.org>
2011-12-22 01:55:56 -0700

    update for

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Moviedb::Application.config.secret_token = '395b1847fd7cb101b217e7a13cd3cd688881ad79e79354cdf6b3b7516281ed182fee660f6da0f043cae1cf9b2fcd796011ea9705b542440e31ea66465f0e78b3'
+Moviedb::Application.config.secret_token = '9f634205a9217c6b0d31095f030d5b8f7685fd37a63619bda897208d2753f21acfbbc327fd68bc08ed9c890fa35e096ba911eb5c6fee60ca5864d020e9df5dee'
--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,6 @@ Moviedb::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-	#connect 'movies/search', :controller => 'movies', :action => 'search'
-  resources :movies
-
   # Sample resource route with options:
   #   resources :products do
   #     member do
@@ -51,14 +48,11 @@ Moviedb::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
-  root :to => "application#home"
-
+  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  match ':controller(/:action(/:id(.:format)))'
+  # match ':controller(/:action(/:id(.:format)))'
 end
