0bb6c100f63313fa0a67dfa5c674507531628c26
Kevin <kev@drule.org>
2014-08-12 19:48:45 -0600

    checkpoint, page loads

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 gem 'therubyracer',  platforms: :ruby
 
+#gem 'less-rails'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require twitter/bootstrap
-//= require jquery/bootstrap
-//= require gritter
-//= require_tree .
+//# require jquery
+//# require jquery/bootstrap
+//# require gritter
+//# require_tree .
