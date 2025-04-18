280e88d3e22f11c95825953820b37f46cef3ae3d
Kevin Bennett <kev@drule.org>
2025-04-18 14:00:48 +0000

    scaffold movies model

---
+++ db/migrate/20250418135943_create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration[7.2]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :description
+      t.integer :release_date
+
+      t.timestamps
+    end
+  end
+end
