008bbc08093c545b511f2c3fdc14ec1753039c67
Kevin Bennett <kev@drule.org>
2025-04-18 06:16:01 +0000

    add yarn and movie scaffold

---
+++ spec/views/movies/index.html.erb_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "movies/index", type: :view do
+  before(:each) do
+    assign(:movies, [
+      Movie.create!(
+        title: "Title",
+        description: "MyText"
+      ),
+      Movie.create!(
+        title: "Title",
+        description: "MyText"
+      )
+    ])
+  end
+
+  it "renders a list of movies" do
+    render
+    cell_selector = 'div>p'
+    assert_select cell_selector, text: Regexp.new("Title".to_s), count: 2
+    assert_select cell_selector, text: Regexp.new("MyText".to_s), count: 2
+  end
+end
