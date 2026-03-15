5c7a7e7e557054558cfa3b61ccd269067538064b
Kevin Bennett <kevin.bennett@firstup.io>
2026-03-15 01:02:47 -0600

    fix running local server

--- config/puma.rb
+++ config/puma.rb
@@ -5,7 +5,6 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 threads threads_count, threads_count
 
-port ENV.fetch("PORT", 3000)
 bind "tcp://0.0.0.0:#{ENV.fetch('PORT', 3000)}"
 
 environment ENV.fetch("RAILS_ENV", "development")
